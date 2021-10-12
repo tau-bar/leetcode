639a4aa3b404fcdd369dc6a5709e475f903b543b
Taufiq Abdul Rahman <tau-bar@users.noreply.github.com>
2021-10-12 21:41:09 +0800

    Update 576

--- arrays/easy/576-ReshapeMatrix/Solution.java
+++ arrays/easy/576-ReshapeMatrix/Solution.java
@@ -10,16 +10,12 @@ class Solution {
             int[][] newMat = new int[r][c];
             for (int i = 0; i < oldRows; i++) {
                 for (int j = 0; j < oldCols; j++) {
-                    if (colPointer == 0) {
-                        newMat[rowPointer] = new int[c];
-                    }
                     if (colPointer < c) {
                         newMat[rowPointer][colPointer] = mat[i][j];
                         colPointer++;
                     } else {
                         colPointer = 1;
                         rowPointer++;
-                        newMat[rowPointer] = new int[c];
                         newMat[rowPointer][0] = mat[i][j];
                     }
                 }
